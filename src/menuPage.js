f130bafdb6c5d1d75707103c6ef3e778d453e5f2
Shalom S. De Veyra <191439611+colmelom0410@users.noreply.github.com>
2025-06-09 13:21:12 +0800

    Refactor to Use Data-Driven Menu Structure

--- src/menuPage.js
+++ src/menuPage.js
@@ -57,54 +57,56 @@ function loadPage(){
     const menuTitleH1 = document.createElement('h1');
     menuTitleH1.textContent = "MENU"
     const menuTitleH3 = document.createElement('h3');
-    menuTitleH3.textContent = "Kainan ni Shalom";
+    menuTitleH3.textContent = "ShaMae";
 
     // foods
     const divFoods = document.createElement('div');
-    divFoods.id = "divFoods";   
-
-    //Breakfast Menu
-    const MenuTemplate1 = new MenuTemplate("BREAKFAST");
-    const menuBlock1 = MenuTemplate1.createTemplate();
-    const bfMeal1 = new Meal("Tapsilog", "Php 130");
-    const bfMeal2 = new Meal("Tocilog", "Php 125");
-    const bfMeal3 = new Meal("Longsilog", "Php 125");
-    const bfMeal4 = new Meal("Hotsilog", "Php 120");
-    divFoods.appendChild(menuBlock1);
-    menuBlock1.appendChild(bfMeal1.createP());
-    menuBlock1.appendChild(bfMeal2.createP());
-    menuBlock1.appendChild(bfMeal3.createP());
-    menuBlock1.appendChild(bfMeal4.createP());
+    divFoods.id = "divFoods";
     
-    //Snacks
-    const MenuTemplate2 = new MenuTemplate("SNACKS (Good For 2)");
-    const menuBlock2 = MenuTemplate2.createTemplate();
-    const snacks1 = new Meal("Pancit Palabok", "Php 129");
-    const snacks2 = new Meal("Spaghetti Platter", "Php 119");
-    const snacks3 = new Meal("Cheesy Macaroni", "Php 129");
-    const snacks4 = new Meal("Pancit Lomi", "Php 109");
-    divFoods.appendChild(menuBlock2);
-    menuBlock2.appendChild(snacks1.createP());
-    menuBlock2.appendChild(snacks2.createP());
-    menuBlock2.appendChild(snacks3.createP());
-    menuBlock2.appendChild(snacks4.createP());
+    const menuSelections = [{
+        title:"BREAKFAST",
+        meals:[
+            ["Tapsilog", "Php 130"],
+            ["Tocilog", "Php 125"],
+            ["Longsilog", "Php 125"],
+            ["Hotsilog", "Php 120"],
+        ]
+    },
+    {
+        title:"SNACKS (Good For 2)",
+        meals:[
+            ["Pancit Palabok", "Php 129"],
+            ["Spaghetti Platter", "Php 119"],
+            ["Cheesy Macaroni", "Php 129"],
+            ["Pancit Lomi", "Php 109"],
+        ]
 
-    //Meat Dishes
-    const MenuTemplate3 = new MenuTemplate("MEAT DISHES");
-    const menuBlock3 = MenuTemplate3.createTemplate();
-    const meat1 = new Meal("Beef kare-kare", "Php 259");
-    const meat2 = new Meal("Chicken Spicy Inasal", "Php 149");
-    const meat3 = new Meal("Pork Crispy Lumpia", "Php 119");
-    const meat4 = new Meal("Pork Liempo", "Php 189");
-    divFoods.appendChild(menuBlock3);
-    menuBlock3.appendChild(meat1.createP());
-    menuBlock3.appendChild(meat2.createP());  
-    menuBlock3.appendChild(meat3.createP());
-    menuBlock3.appendChild(meat4.createP());
+    },{
+        title:"MEAT DISHES",
+        meals:[
+            ["Beef kare-kare", "Php 259"],
+            ["Chicken Spicy Inasal", "Php 149"],
+            ["Pork Crispy Lumpia", "Php 119"],
+            ["Pork Liempo", "Php 189"],
+        ]
+    }]
 
+    menuSelections.forEach(index => {
+        const template = new MenuTemplate(index.title);
+        const menuBlock = template.createTemplate();
+        index.meals.forEach(([food,price])=>{
+            const meal = new Meal(food, price);
+            menuBlock.appendChild(meal.createP());
+        });
+        divFoods.appendChild(menuBlock);
+    })
+
+
+    //text footer
     const menuFooter = document.createElement('h2');
     menuFooter.id = "menuFooter";
     menuFooter.textContent = "Tasty Filipino Food for Everyone!";
+
     // Images
     const foodItem1 = document.createElement('img');
     foodItem1.src = food1;
